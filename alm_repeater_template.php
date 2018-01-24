9cfeb12036191e210142b0f754b53b9b0fc37b7c
childressdev <jcampbell@childressagency.com>
2018-01-24 11:32:10 -0500

    added

---
+++ alm_repeater_template.php
@@ -0,0 +1,7 @@
+<li>
+  <article class="post">
+    <h3 class="post-date">Posted on <?php echo get_the_date('F j, Y'); ?></h3>
+    <h2 class="post-title"><?php the_title(); ?></h2>
+    <?php the_content(); ?>
+  </article>
+</li>
---
+++ searchform.php
@@ -0,0 +1,9 @@
+<form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
+  <div class="input-group">
+    <label for="s" class="sr-only">Search</label>
+    <input type="search" id="s" name="s" class="form-control" placeholder="Search..." />
+    <span class="input-group-btn">
+      <button type="submit" class="btn-main"><i class="fa fa-search"></i></button>
+    </span>
+  </div>
+</form>
